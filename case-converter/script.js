12c2250f3b7c8b29c0d2c66f65f43fac59e58a91
Vladimir <mr.katenin@mail.ru>
2022-03-05 17:39:24 +0300

    feat: add download btn

--- case-converter/script.js
+++ case-converter/script.js
@@ -24,6 +24,19 @@ function handleText(event) {
             .map((sentence) => sentence.trim()[0].toUpperCase() + sentence.slice(1).toLowerCase());
         textarea.value = newTextArr.join('. ').trim();
     }
+    if (event.target.id === 'save-text-file') {
+        download('text.txt', textarea.value);
+    }
+}
+
+function download(filename, text) {
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
 }
 
 document.querySelector('.btn-container').addEventListener('click', handleText)
